1d38112773c09f94d92dcaf1ff2cf8350d3f1ffb
dinhquy96 <39159800+dinhquy96@users.noreply.github.com>
2019-06-09 17:42:45 +0700

    a
    a

--- login.js
+++ login.js
@@ -6,6 +6,32 @@
 
         if(email == 'admin@gmail.com' && pass == 'hanu98' ){
           sessionStorage.setItem("email",email);
+           sessionStorage.setItem("status",'10');
+
+          window.location = 'https://hanu98.github.io/Admin/index1.html';
+        }
+        if(email == 'trangem@gmail.com' && pass == 'hanu98' ){
+          sessionStorage.setItem("email",email);
+           sessionStorage.setItem("status",'2');
+
+          window.location = 'https://hanu98.github.io/Admin/index1.html';
+        }
+        if(email == 'lanem@gmail.com' && pass == 'hanu98' ){
+          sessionStorage.setItem("email",email);
+           sessionStorage.setItem("status",'2');
+
+          window.location = 'https://hanu98.github.io/Admin/index1.html';
+        }
+        if(email == 'hueem@gmail.com' && pass == 'hanu98' ){
+          sessionStorage.setItem("email",email);
+           sessionStorage.setItem("status",'1');
+
+          window.location = 'https://hanu98.github.io/Admin/index1.html';
+        }
+         if(email == 'linhmiu@gmail.com' && pass == 'hanu98' ){
+          sessionStorage.setItem("email",email);
+           sessionStorage.setItem("status",'10');
+
           window.location = 'https://hanu98.github.io/Admin/index1.html';
         }
     }
